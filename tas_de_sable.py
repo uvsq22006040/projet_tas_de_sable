016971bfd0e420e3ead5f739946aa2a500b0b65c
Utilisateur <user@debian.arcahe.ovh>
2022-02-08 11:44:41 +0100

    nom corrigé

--- tas_de_sable.py
+++ tas_de_sable.py
@@ -2,7 +2,7 @@
 # groupe MIASHS TD 02
 # Michael GREKOBI
 # Pierre-Axel RIVIERE
-# Jeremy  SALEH
+# Jeremy SALEH
 # Ilyes MAGHLAOUA
 # https://github.com/uvsq22006040/projet_tas_de_sable
 ####################################
